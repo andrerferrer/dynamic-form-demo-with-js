d345de6ac76faeef90801fdb4a85a96f9810447a
Andre Ferrer <andrerferrer@hotmail.com>
2020-10-02 01:36:00 -0300

    Handle the new restaurants in the controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,8 +13,23 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.new strong_params
-    @restaurant.save ? redirect_to(@restaurant) : render(:new)
+    restaurants_data = strong_params
+    @restaurants = Restaurant.create(restaurants_data)
+    @restaurants = @restaurants.filter { |restaurant| restaurant.errors.any? }
+
+    if @restaurants.empty?
+      redirect_to restaurants_path, alert: "All restaurants where correctly created!"
+    else
+      # check if any restaurants were saved and show the according message
+      if @restaurants.count != restaurants_data.count
+        flash[:alert] = "Some restaurants were saved! You should check in on some of the fields below."
+      else
+        flash[:alert] = "You should check in on some of the fields below."
+      end
+      
+      render :new
+    end
+
   end
 
   def destroy
@@ -29,6 +44,6 @@ class RestaurantsController < ApplicationController
   end
 
   def strong_params
-    params.require(:restaurant).permit(Restaurant::STRONG_PARAMS)
+    params.require(:restaurants).map { |param| param.permit(Restaurant::STRONG_PARAMS) }
   end
 end
